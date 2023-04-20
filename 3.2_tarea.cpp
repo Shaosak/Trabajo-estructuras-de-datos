8511bb623376c7f0c4fb37fe44b35cd83426e543
Luca121p <116899367+Luca121p@users.noreply.github.com>
2023-04-19 23:29:09 -0400

    Update 3.2_tarea.cpp

--- 3.2_tarea.cpp
+++ 3.2_tarea.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int i;
 
 int* comprarTarjeta(string nombre, int dia, int &m) {
+	//recibe parametros del nombre de la persona que quiere comprar, el dia en que se compra la tarjeta
+	//y el tamaño del arreglo que se va a retornar. En la funcion m correspondera al largo del nombre, ya
+	//que sera el tamaño del arreglo a retornar. Usando memoria dinamica creamos el arreglo a retornar, donde
+	//a travez de un for se agregaran en la posicion i del arreglo el valor de nombre[i]%dia.
 	m = nombre.length();  //m es igual a la longitud del nombre
 	int *ar = new int[m];   //un arreglo dinamico
 	for(int i =0; i<m;i++){
